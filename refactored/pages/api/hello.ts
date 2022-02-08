5e94141e6a733f3abb27d21537fe4f9ef5b928f4
BE-CH <mikkel.l.bech@gmail.com>
2022-02-08 14:44:00 +0100

    + creates new user on hello

--- refactored/pages/api/hello.ts
+++ refactored/pages/api/hello.ts
@@ -2,12 +2,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { userInfo } from 'os';
 import connectDB from '../../middleware/mongodb';
+import User from '../../models/User.scheme';
 
 type Data = {
   name: string;
 };
 
-function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const newUser = await new User({
+    email: '123@gmail.com',
+    pw_hash: '1234',
+    user_id: '123',
+    username: 'bech',
+  });
+
+  await newUser.save();
   res.status(200).json({ name: 'John Doe' });
 }
 
