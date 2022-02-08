37402b310f61814760d3730a15099dcd525d5c97
BE-CH <mikkel.l.bech@gmail.com>
2022-02-08 14:36:31 +0100

    + fixed stuff

--- refactored/middleware/mongodb.ts
+++ refactored/middleware/mongodb.ts
@@ -8,7 +8,11 @@ const connectDB = (handler: NextApiHandler) => async (req: NextApiRequest, res: 
     return handler(req, res);
   }
   // Use new db connection
-  await mongoose.connect(process.env.MONGODB_URI!);
+  await mongoose.connect(
+    `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME!}:${
+      process.env.MONGO_INITDB_ROOT_PASSWORD
+    }@database:27017/minitwit`
+  );
   return handler(req, res);
 };
 
