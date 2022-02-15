28bf20f6a312ab23d0e30955a14dfcc8e261f08a
AlexBMJ <AlexBMJ@users.noreply.github.com>
2022-02-15 23:39:43 +0100

    refactor for better readability

--- refactored/pages/api/[username].ts
+++ refactored/pages/api/[username].ts
@@ -7,7 +7,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const user_obj = await get_user({ username: req.query.username[0] });
     if (user_obj && user_obj._id && user_obj.username && user_obj.email) {
         let messages = await Message.find({author_id: user_obj._id})
-        return res.status(200).json({username: user_obj.username, email: user_obj.email, messages: messages});
+        return res.status(200).json({
+            username: user_obj.username, 
+            email: user_obj.email, 
+            messages: messages
+        });
     }
     return res.status(404);
   }
