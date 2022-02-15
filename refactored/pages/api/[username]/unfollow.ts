b392bedde633dba17744167119868e42fb7b7e9e
AlexBMJ <AlexBMJ@users.noreply.github.com>
2022-02-15 23:07:09 +0100

    refactor follow for better readability

--- refactored/pages/api/[username]/unfollow.ts
+++ refactored/pages/api/[username]/unfollow.ts
@@ -4,27 +4,24 @@ import { verify } from 'jsonwebtoken';
 import { Token } from '../../../types/jwt';
 import { Types } from 'mongoose';
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'POST') {
-    return res.status(400);
-  }
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    if (req.headers.authorization) {
+      let token = <Token>verify(req.headers.authorization.split(' ')[1], process.env.tokensecret!);
 
-  if (!req.headers.authorization) {
+      if (token.userid) {
+        const user_obj = await get_user({ username: req.query.username[0] });
+        if (user_obj && user_obj._id) {
+          await unfollow(new Types.ObjectId(token.userid), user_obj._id);
+          return res.status(200).json(user_obj._id);
+        }
+        return res.status(404);
+      }
+      return res.status(401);
+    }
     return res.status(401);
   }
-
-  let token = <Token>verify(req.headers.authorization.split(' ')[1], process.env.tokensecret!);
-
-  if (!token.userid) {
-    return res.status(401);
-  }
-
-  const user_obj = await get_user({ username: req.query.username[0] });
-
-  if (user_obj && user_obj._id) {
-    await unfollow(new Types.ObjectId(token.userid), user_obj._id);
-    return res.status(200).json(user_obj._id);
-  }
-
-  return res.status(404);
+  return res.status(400);
 };
+
+export default handler;
