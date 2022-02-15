d3488ff732afeec2f757ea8ebefe2dd200dad099
Deniz Yildirim <denizyil@outlook.dk>
2022-02-15 22:16:17 +0100

    Bech is a noob

--- refactored/pages/api/public.ts
+++ refactored/pages/api/public.ts
@@ -1,8 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { follow, get_user } from '../../db_helpers/user_helper';
-import { verify } from 'jsonwebtoken';
-import { Token } from '../../types/jwt';
-import { Types } from 'mongoose';
 import Message from '../../models/Message.schema';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -10,6 +6,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(400);
   }
 
-  const messages = await Message.find({}).sort({ pub_date: -1 }).limit(30).exec();
+  const messages = await Message.aggregate()
+    .lookup({
+      from: 'users',
+      localField: 'author_id',
+      foreignField: 'user_id',
+      as: 'users',
+    })
+    .sort({ pub_date: -1 })
+    .limit(30)
+    .exec();
+
   return res.status(200).json(messages);
 };
