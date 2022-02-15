1165ed6e7d818ab8ad9bfb008586319001dbad65
BE-CH <mikkel.l.bech@gmail.com>
2022-02-15 13:20:08 +0100

    + removed from hello.ts

--- refactored/pages/api/hello.ts
+++ refactored/pages/api/hello.ts
@@ -9,14 +9,6 @@ type Data = {
 };
 
 async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const newUser = await new User({
-    email: '123@gmail.com',
-    pw_hash: '1234',
-    user_id: '123',
-    username: 'bech',
-  });
-
-  await newUser.save();
   res.status(200).json({ name: 'John Doe' });
 }
 
