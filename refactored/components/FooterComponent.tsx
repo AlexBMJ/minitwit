6803adf352cde67ecfa7356e8b5c6439993e9eb9
DenizYil <denizyil@outlook.dk>
2022-02-15 12:57:15 +0100

    refactored layout component

---
+++ refactored/components/FooterComponent.tsx
@@ -0,0 +1,15 @@
+
+import styles from '../styles/footer.module.scss';
+
+
+const Footer: React.FunctionComponent = () => {
+
+   return (
+
+    <div className={styles.footer}>
+    MiniTwit &mdash; A Flask Application
+  </div>
+   )
+}
+
+export default Footer
