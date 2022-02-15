daf45612ef25be3d421d0148273e294e5a06b04c
BE-CH <mikkel.l.bech@gmail.com>
2022-02-15 20:44:23 +0100

    + fixed up some code and added new endpoints

--- refactored/components/Layout.component.tsx
+++ refactored/components/Layout.component.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { useState } from 'react';
-import styles from './styles/layout.module.scss';
+import styles from '../styles/layout.module.scss';
 
 const Layout: React.FunctionComponent = () => {
   const g = {
--- refactored/pages/api/login.ts
+++ refactored/pages/api/login.ts
@@ -2,34 +2,41 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import connectDB from '../../middleware/mongodb';
 import User, { TUser } from '../../models/User.scheme';
-
-var jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+import * as jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import { Token } from '../../types/jwt';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-        var decoded = jwt.verify(req.headers.authorization?.split(" ")[1], process.env.tokensecret)
-        let user = await User.findOne({ id: decoded.id })
-        if (!user) return res.status(500).json({ message: 'User not found!' })
-        return res.json({ user: user })
+  if (req.method === 'GET') {
+    if (req.headers.authorization) {
+      var decoded = <Token>jwt.verify(req.headers.authorization?.split(' ')[1], process.env.TOKEN_SECRET!);
+      let user = await User.findById(decoded.userid);
+      if (user) {
+        return res.json({ user: user });
+      }
     }
-    else if (req.method === 'POST') {
-        if (!req.body.username) return res.status(400).json({ message: 'Username is required!' })
 
-        let user = await User.findOne({ username: req.body.username })
-        if (!user) return res.status(500).json({ message: 'User not found!' })
+    return res.status(500).json({ message: 'User not found!' });
+  } else if (req.method === 'POST') {
+    if (req.body.username) {
+      const user = await User.findOne({ username: req.body.username });
 
-        bcrypt.compare(req.body.password, user.pw_hash).then(function(result : any) {
-            if (result) {
-                let token = jwt.sign({ userid: user?.id }, process.env.tokensecret)
-                return res.json({ token: token, message: `Logged in as ${user?.username}.` })
-            }
-            return res.status(400).json({ message: 'Incorrect password!' })
+      if (user) {
+        bcrypt.compare(req.body.password, user.pw_hash).then(function (result: any) {
+          if (result) {
+            let token = jwt.sign({ userid: user?.id }, process.env.TOKEN_SECRET!);
+            return res.json({ token: token, message: `Logged in as ${user?.username}.` });
+          }
+          return res.status(400).json({ message: 'Incorrect password!' });
         });
+      }
+      return res.status(500).json({ message: 'User not found!' });
     }
-    else {
-        return res.status(400).json({ message: 'Bad request!' })
-    }
+
+    return res.status(400).json({ message: 'Username is required!' });
+  } else {
+    return res.status(400).json({ message: 'Bad request!' });
+  }
 }
 
 export default connectDB(handler);
