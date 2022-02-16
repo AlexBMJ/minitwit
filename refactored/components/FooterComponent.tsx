2ed0276003dbb905d6368e9541da5ad8162a43d7
AlexBMJ <AlexBMJ@users.noreply.github.com>
2022-02-16 02:14:04 +0100

    move refactored to root

--- refactored/components/FooterComponent.tsx
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/footer.module.scss';
-
-const Footer: React.FunctionComponent = () => {
-  return <div className="footer">MiniTwit &mdash; A Flask Application</div>;
-};
-
-export default Footer;
--- refactored/types/userInfo.ts
+++
@@ -1,3 +0,0 @@
-import { TMessage } from '../models/Message.schema';
-
-export type UserInfo = { username: string; email: string; messages: TMessage[] };
