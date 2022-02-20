23bf54ca80b580c12420b638160c18015c7164f9
Alex <AlexBMJ@users.noreply.github.com>
2022-02-20 23:55:30 +0100

    error handling for incorrect username

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -6,33 +6,37 @@ import authenticate, { AuthRequest } from '../../middleware/authentication';
 
 async function handler(req: AuthRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    const { username, email, password } = req.body;
+    const { username, email, password }: { username: string; email: string; password: string } = req.body;
 
-    if (username && email && password) {
-      const user = await User.findOne({ email: email });
-      if (!user) {
-        bcrypt.genSalt(10, async function (err, salt) {
-          if (!err) {
-            bcrypt.hash(password, salt, async function (err, hash) {
-              if (!err) {
-                const newUser = await new User({ email: email, pw_hash: hash, username: username }).save();
-                let token = jwt.sign({ userid: newUser.id }, process.env.TOKEN_SECRET!);
-                return res.status(200).json({ token: token, message: `Logged in as ${newUser.username}.` });
-              } else {
-                console.log(err);
-                return res.status(500).json({ message: 'We could not generate a hash!' });
-              }
-            });
-          } else {
-            console.log(err);
-            return res.status(500).json({ message: 'We could not generate a salt!' });
-          }
-        });
+    if (!username.includes(':')) {
+      if (username && email && password) {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+          bcrypt.genSalt(10, async function (err, salt) {
+            if (!err) {
+              bcrypt.hash(password, salt, async function (err, hash) {
+                if (!err) {
+                  const newUser = await new User({ email: email, pw_hash: hash, username: username }).save();
+                  let token = jwt.sign({ userid: newUser.id }, process.env.TOKEN_SECRET!);
+                  return res.status(200).json({ token: token, message: `Logged in as ${newUser.username}.` });
+                } else {
+                  console.log(err);
+                  return res.status(500).json({ message: 'We could not generate a hash!' });
+                }
+              });
+            } else {
+              console.log(err);
+              return res.status(500).json({ message: 'We could not generate a salt!' });
+            }
+          });
+        } else {
+          return res.status(400).json({ message: 'User with that email was already found!' });
+        }
       } else {
-        return res.status(400).json({ message: 'User with that email was already found!' });
+        return res.status(400).json({ message: 'Username, email and password must be set!' });
       }
     } else {
-      return res.status(400).json({ message: 'Username, email and password must be set!' });
+      return res.status(400).json({ message: 'Username cannot contain the colon character' });
     }
   } else if (req.method === 'GET') {
     if (req.authenticated) {
