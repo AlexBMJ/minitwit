ded69d2c3654a46a6d375241d9ac816491100c90
Deniz Isik <denizisik4600@gmail.com>
2022-02-24 23:49:18 +0100

    Fix small issue with if statement when checking for NaN

--- pages/api/msgs.ts
+++ pages/api/msgs.ts
@@ -6,7 +6,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const amount = <string>req.query.amount;
     const numberAmount = Number(amount);
 
-    if (numberAmount !== NaN) {
+    if (!isNaN(numberAmount)) {
       const recentMessages = await Message.find({}).limit(numberAmount).sort({ pub_date: -1 }).exec();
 
       return res.status(200).json({ messages: recentMessages });
