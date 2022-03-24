82e3b4da79f6ce1fb84997cb6c88dc49ee46433a
DenizYil <denizyil@outlook.dk>
2022-03-24 18:21:49 +0100

    start work on logging

--- middleware/MiniTwitRoute.ts
+++ middleware/MiniTwitRoute.ts
@@ -3,6 +3,7 @@ import { TUser } from '../models/User.scheme';
 import setLatest from '../helpers/latest_helper';
 import Prometheus, { Histogram } from 'prom-client';
 import httpRequestDurationMilliseconds from '../helpers/metrics_helper';
+import logger from '../app/logger';
 
 export interface AuthRequest extends NextApiRequest {
   user?: TUser;
@@ -11,13 +12,14 @@ export interface AuthRequest extends NextApiRequest {
 
 const MiniTwitRoute =
   (handler: NextApiHandler, routes: string[], endpoint: string) => async (req: AuthRequest, res: NextApiResponse) => {
+    logger.info(`Received request: [${req.method}] ${req.url}`, {test: 'hello'});
+
     if (!routes.includes(req.method!)) {
       return res.status(405).json({ message: 'Method not accepted!' });
     }
     setLatest(req);
 
     const foundMetric: any = await httpRequestDurationMilliseconds();
-    console.log(foundMetric);
     const timer = foundMetric.startTimer();
     const result = handler(req, res);
     timer({ route: endpoint, method: req.method, status_code: res.statusCode });
