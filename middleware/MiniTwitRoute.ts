b21484d1da86620f6e07c35a38b4246486b72976
DenizYil <denizyil@outlook.dk>
2022-03-24 18:49:32 +0100

    Add logging for all routes

--- middleware/MiniTwitRoute.ts
+++ middleware/MiniTwitRoute.ts
@@ -10,18 +10,54 @@ export interface AuthRequest extends NextApiRequest {
   authenticated?: boolean;
 }
 
-const MiniTwitRoute =
-  (handler: NextApiHandler, routes: string[], endpoint: string) => async (req: AuthRequest, res: NextApiResponse) => {
-    logger.info(`Received request: [${req.method}] ${req.url}`, {test: 'hello'});
+const MiniTwitRoute = (handler: NextApiHandler, routes: string[], endpoint: string) => async (req: AuthRequest, res: NextApiResponse) => {
+  
+    logger.info( {
+      method: req.method,
+      url: req.url,
+      endpoint: endpoint,
+      body: req.body,
+      query: req.query
+    }, `Received request: [${req.method}] ${req.url}`);
 
     if (!routes.includes(req.method!)) {
+      logger.info({
+        method: req.method,
+        url: req.url,
+        endpoint,
+        body: req.body,
+        query: req.query
+      }, `Unaccepted method received for: [${req.method}] ${req.url}`);
       return res.status(405).json({ message: 'Method not accepted!' });
     }
+    
     setLatest(req);
 
     const foundMetric: any = await httpRequestDurationMilliseconds();
     const timer = foundMetric.startTimer();
-    const result = handler(req, res);
+
+    let result;
+    
+    try {
+      result = await handler(req, res);
+      logger.info({
+        method: req.method,
+        url: req.url,
+        endpoint,
+        body: req.body,
+        query: req.query,
+      }, `Handler executed successfully for ${endpoint}`)
+    }
+    catch(ex) {
+      logger.error( {
+        method: req.method,
+        url: req.url,
+        endpoint,
+        body: req.body,
+        query: req.query
+      }, `Exception received for handler: ${ex}`,)
+    }
+
     timer({ route: endpoint, method: req.method, status_code: res.statusCode });
     return result;
   };
