a5e18953cbb92043e3f1856a9bef3e4f80b29e03
Alex <33891167+AlexBMJ@users.noreply.github.com>
2022-04-06 03:01:02 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 svn checkout https://github.com/AlexBMJ/minitwit/trunk/config
+wget https://github.com/AlexBMJ/minitwit/raw/main/deployment.yml
 sudo docker stack deploy -c deployment.yml minitwit
