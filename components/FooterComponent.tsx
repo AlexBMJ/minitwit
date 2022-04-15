dd9f18e8d132451dd5cfa7a7cf7254e8d5975f02
Alex <33891167+AlexBMJ@users.noreply.github.com>
2022-04-15 23:17:46 +0200

    change color scheme

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -1,4 +1,4 @@
-import styles from '../styles/layout.module.scss';
+import styles from '../styles/footer.module.scss';
 
 const Footer: React.FunctionComponent = () => {
   return <div className={styles.footer}>MiniTwit &mdash; A Next.js Application</div>;
